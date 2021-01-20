d8b31a7521b45ea35f6c607d9d747b39bb95fa2b
Tanya <77443212+Tanya00001@users.noreply.github.com>
2021-01-20 10:40:36 +0530

    Update Linear_Regression_Model.R

--- Linear_Regression_Model.R
+++ Linear_Regression_Model.R
@@ -6,17 +6,17 @@ salary1
 #To plot this....
 plot(salary1)
     #OR
-plot(salary1$salary,salary1$year_of_education) #Through $ sign we can access specific column in a dataset.
+plot(salary1$salary,salary1$year_of_experience) #Through $ sign we can access specific column in a dataset.
 
 #Linear regression....
 #Equation y=mx+c where y works as response variable, x works as input variable, m is slope and c is the intercept.
 
-lm(salary1$salary ~ salary1$year_of_education)#Through this you will get intercept and slope.
-#Here lm is linear model and ~ specifies that salary regress on year_of_education.
+lm(salary1$salary ~ salary1$year_of_experience) #Through this you will get intercept and slope.
+#Here lm is linear model and ~ specifies that salary regress on year_of_experience.
 
 #NOW LET'S CHECK IS THIS MODEL PREDICTING ACCURATELY.
 #My dataset has these values....
-#year_of_education    salary
+#year_of_experience    salary
 
 # 2                	50,000
 # 4               	70,000
