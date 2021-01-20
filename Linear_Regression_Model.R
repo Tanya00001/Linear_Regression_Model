79b9708e9a455c1f2eb3a6632004420a922d5cdb
Tanya <77443212+Tanya00001@users.noreply.github.com>
2021-01-20 10:57:34 +0530

    Update Linear_Regression_Model.R

--- Linear_Regression_Model.R
+++ Linear_Regression_Model.R
@@ -6,26 +6,26 @@ salary1
 #To plot this....
 plot(salary1)
     #OR
-plot(salary1$salary,salary1$year_of_experience) #Through $ sign we can access specific column in a dataset.
+plot(salary1$salary,salary1$years_of_experience) #Through $ sign we can access specific column in a dataset.
 
 #Linear regression....
 #Equation y=mx+c where y works as response variable, x works as input variable, m is slope and c is the intercept.
 
-lm(salary1$salary ~ salary1$year_of_experience) #Through this you will get intercept and slope.
-#Here lm is linear model and ~ specifies that salary regress on year_of_experience.
+lm(salary1$salary ~ salary1$years_of_experience) #Through this you will get intercept and slope.
+#Here lm is linear model and ~ specifies that salary regress on years_of_experience.
 
 #NOW LET'S CHECK IS THIS MODEL PREDICTING ACCURATELY.
 #My dataset has these values....
-#year_of_experience    salary
+#years_of_experience    salary
 
-# 2                	50,000
-# 4               	70,000
-# 7               	100000
-# 9               	300000
-# 3               	55,000
-# 1               	20,000
-# 5               	80,000
-# 6	                90,000
+# 2                 	50,000
+# 4                 	70,000
+# 7                 	100000
+# 9                 	300000
+# 3                 	55,000
+# 1                 	20,000
+# 5                   	80,000
+# 6	                    90,000
 
 # intercept(c)=-31128
 # slope(m)=27406
@@ -34,10 +34,10 @@ lm(salary1$salary ~ salary1$year_of_experience) #Through this you will get inter
 
 #y=mx+c, x=4(have taken randomly)
 (27406*4)+(-31128)
-#model predicted, salary=78496
-#Actual, salary=	70,000
+#model predicted, salary=78,496
+#Actual,          salary=70,000
 
-#YOU CAN ALSO CHECK ALL OTHER INPUT VARIABLE(YEAR_OF_EXPERIENCE).
+#YOU CAN ALSO CHECK ALL OTHER INPUT VARIABLE(YEARS_OF_EXPERIENCE).
 #IF LINEAR REGRESSION MODEL IS NOT PREDICTING ACCURATELY THEN THIS IS AN ERROR! IN MODEL.
 #YOU CAN OPT FOR ANY OTHER MODEL, BASED ON YOUR DATASET.
 
